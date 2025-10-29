500dc2b519933183bbe4903a2b57c8ea7922d455
Luffyconqueredhaki <137677359+Luffyconqueredhaki@users.noreply.github.com>
2025-10-29 01:15:17 +0100

    Refactor tests for preprocessing functions

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,19 +2,19 @@
 from src.preprocessing import preprocess_social
 
 def test_rimozione_mention_url():
-    """Testa che @mention e URL siano sostituiti."""
+    #Testa che @mention e URL siano sostituiti
     testo = "Ciao @amico, guarda https://sito.com"
     atteso = "Ciao @user, guarda http"
     assert preprocess_social(testo) == atteso
 
-def test_gestione_emoji_hashtag():
-    """Testa che emoji e hashtag siano gestiti."""
-    testo = "cool #MLOps"
-    atteso = "Like MLOps :rocket:"
+def test_gestione_hashtag():
+    #Testa che gli hashtag siano gestiti.
+    testo = "Adoro #MLOps"
+    atteso = "Adoro MLOps"
     assert preprocess_social(testo) == atteso
 
 def test_testo_pulito():
-    """Testa un testo che non deve cambiare."""
+    #Testa un testo che non deve cambiare
     testo = "Un testo normale."
     atteso = "Un testo normale."
     assert preprocess_social(testo) == atteso
