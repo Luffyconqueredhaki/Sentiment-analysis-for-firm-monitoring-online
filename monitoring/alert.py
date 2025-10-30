8c7b4876dab616c8ed0b6f9734f065b55cce2751
Luffyconqueredhaki <137677359+Luffyconqueredhaki@users.noreply.github.com>
2025-10-30 15:56:16 +0100

    Update metrics file path in alert.py

--- monitoring/alert.py
+++ monitoring/alert.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 THRESHOLD_F1 = 0.70
-METRICS_FILE = "monitoring/metrics/latest_metrics.csv"
-
+METRICS_FILE = "monitoring/metrics.csv"
 df = pd.read_csv(METRICS_FILE)
 latest_f1 = df['f1'].iloc[-1]
 
